eff558f3d79fb23c6aac8f5a174d6a1cdad3b3dc
chtti <peter@chtti>
2019-04-12 10:36:08 +0800

    implement static and block

---
+++ src/com/cht/training/Lab8/OwnAccountant.java
@@ -0,0 +1,11 @@
+package com.cht.training.Lab8;
+
+public class OwnAccountant {
+    public void calculate(){
+        Employee e1 = new Employee();
+        System.out.println("a=" + e1.a);
+        System.out.println("a=" + e1.b);
+        System.out.println("a=" + e1.c);
+//        System.out.println("a=" + e1.d);
+    }
+}
---
+++ src/com/cht/training/Lab8d/Customer.java
@@ -0,0 +1,14 @@
+package com.cht.training.Lab8d;
+
+import com.cht.training.Lab8.Employee;
+
+public class Customer {
+    public void foo() {
+        Employee e1 = new Employee();
+        System.out.println("a=" + e1.a);
+//        System.out.println("a=" + e1.b);
+//        System.out.println("a=" + e1.c);
+//        System.out.println("a=" + e1.d);
+
+    }
+}
