ab27c9e5a6c5900ac65b75283b12a2d2aee229d9
chtti <peter@chtti>
2019-04-12 16:08:19 +0800

    pattern, matcher, regex

--- src/com/cht/training/Main22.java
+++ src/com/cht/training/Main22.java
@@ -1,5 +1,8 @@
 package com.cht.training;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main22 {
     private  static final String S1 = "Hello world  java   programming";
     private  static final String S2 = "Hello_world__java___programming";
@@ -7,10 +10,43 @@ public class Main22 {
     private  static final String S4 = "Hello/world//java///programming";
 
     public static void main(String[] args) {
-        for(String segment:S1.split(" ")){
-            System.out.printf("[%s] ", segment);
+        Pattern p1 = Pattern.compile("\\w+");
+        Matcher matcher1 = p1.matcher(S1);
+        while (matcher1.find()){
+            System.out.printf("(%s) ", matcher1.group());
+        }
+
+        System.out.printf("\n");
+
+        Pattern p2 = Pattern.compile("[^\\s]+");
+        Matcher matcher2 = p2.matcher(S1);
+        while (matcher2.find()){
+            System.out.printf("(%s) ", matcher2.group());
         }
         System.out.printf("\n");
+
+        Pattern p3 = Pattern.compile("[^_]+");
+        Matcher matcher3 = p3.matcher(S2);
+        while (matcher3.find()){
+            System.out.printf("(%s) ", matcher3.group());
+        }
+        System.out.printf("\n");
+
+        Pattern p4 = Pattern.compile("[^\\\\]+");
+        Matcher matcher4 = p4.matcher(S3);
+        while (matcher4.find()){
+            System.out.printf("(%s) ", matcher4.group());
+        }
+        System.out.printf("\n");
+
+        Pattern p5 = Pattern.compile("[^/]+");
+        Matcher matcher5 = p5.matcher(S4);
+        while (matcher5.find()){
+            System.out.printf("(%s) ", matcher5.group());
+        }
+        System.out.printf("\n");
+
+
 
         for(String segment:S2.split("_")){
             System.out.printf("[%s] ", segment);
